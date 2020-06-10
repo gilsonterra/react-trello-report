cf422c9a2b0c66cd36d2261456b2ef5e0c4c10fd
Gilson Terra <gilsonterra@gmail.com>
2020-06-10 20:39:52 -0300

    change directory

---
+++ build/precache-manifest.632ec2dfeb9d4b9a7a5538d539c1972d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d4fae76849afc299d6c025c018be972",
+    "url": "/react-trello-report/index.html"
+  },
+  {
+    "revision": "714e3e31aa66cbcc2fb5",
+    "url": "/react-trello-report/static/css/2.788bf961.chunk.css"
+  },
+  {
+    "revision": "714e3e31aa66cbcc2fb5",
+    "url": "/react-trello-report/static/js/2.b161aff1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-trello-report/static/js/2.b161aff1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d55ca1e67378a568006",
+    "url": "/react-trello-report/static/js/main.fe1b627b.chunk.js"
+  },
+  {
+    "revision": "58fd0c9d36142efb6eb7",
+    "url": "/react-trello-report/static/js/runtime-main.618e97ba.js"
+  }
+]);
